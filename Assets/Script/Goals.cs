0f951e11fc4e9356679377b7777fc385cf990164
kananon <kananon.char@bumail.net>
2026-03-28 22:04:16 +0700

    Finish

--- Assets/Script/Goals.cs
+++ Assets/Script/Goals.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
 {
+    public GameObject winUI; // ลาก Panel หรือ Text ของ UI Win มาที่นี่
+
+    private void Start()
+    {
+        if (winUI != null)
+            winUI.SetActive(false); // ซ่อน UI ตอนเริ่มเกม
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("You Win!");
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+            if (winUI != null)
+            {
+                winUI.SetActive(true); // แสดง UI Win
+            }
         }
     }
 }
--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Goplay : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
