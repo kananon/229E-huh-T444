91c1ccc40290121700a4a93a347e913383f71af1
kananon <kananon.char@bumail.net>
2026-03-28 18:37:52 +0700

    UPD

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveForce = 10f;
+    public float jumpForce = 7f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v);
+        rb.AddForce(move * moveForce);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
